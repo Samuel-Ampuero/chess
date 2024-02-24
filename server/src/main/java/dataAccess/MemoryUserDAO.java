5a0fe5cc92e786d8ad3a0f22deb7d75188218000
Samuel-Ampuero <sampuero@byu.edu>
2024-02-24 11:21:51 -0700

    1st attempt on MemoryUserDAO

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -1,4 +1,31 @@
 package dataAccess;
 
-public class MemoryUserDAO{
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MemoryUserDAO implements UserDAO{
+    final private Collection<UserData> userDatas = new ArrayList<>();
+    public void createUser(String username, String password, String email) throws DataAccessException {
+        UserData userData = new UserData(username, password, email);
+        userDatas.add(userData);
+    }
+
+    public Collection<UserData> listUsers() throws DataAccessException{
+        return userDatas;
+    }
+
+    public String getUser(String username) throws DataAccessException{
+        for (UserData elem : userDatas){
+            if(elem.username() == username){
+                return elem.username();
+            }
+        }
+        return null;
+    }
+
+    public void deleteAllUsers() throws DataAccessException{
+        userDatas.clear();
+    }
 }
--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -5,13 +5,11 @@ import model.UserData;
 import java.util.Collection;
 
 public interface UserDAO {
-    UserData createUser(String username, String password) throws DataAccessException;
+    void createUser(String username, String password, String email) throws DataAccessException;
 
     Collection<UserData> listUsers() throws DataAccessException;
 
-    UserData getUser(String username) throws DataAccessException;
-
-    void deleteUser(String username) throws DataAccessException;
+    String getUser(String username) throws DataAccessException;
 
     void deleteAllUsers() throws DataAccessException;
 }
